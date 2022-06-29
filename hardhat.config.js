a44c7e49abbcfd7d50eaedeea528d943e4e8bbb9
Vladimir Ivanov <v.ivanov@azuro.org>
2022-06-29 11:13:33 +0300

    add sokol and gnosis chain networks to deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,11 @@ require("web3");
 require("dotenv").config();
 
 const ALCHEMY_API_KEY_RINKEBY = process.env.ALCHEMY_API_KEY_RINKEBY || "";
+const ALCHEMY_API_KEY_KOVAN = process.env.ALCHEMY_API_KEY_KOVAN || "";
+const KOVAN_PRIVATE_KEY = process.env.KOVAN_PRIVATE_KEY || "";
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY || "";
+const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY || "";
+const SOKOL_PRIVATE_KEY = process.env.SOKOL_PRIVATE_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -35,6 +39,34 @@ if (ALCHEMY_API_KEY_RINKEBY != "" && RINKEBY_PRIVATE_KEY != "") {
     url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY_RINKEBY}`,
     accounts: [`${RINKEBY_PRIVATE_KEY}`],
   };
+}
+
+if (ALCHEMY_API_KEY_KOVAN != "" && KOVAN_PRIVATE_KEY != "") {
+  exportNetworks["kovan"] = {
+    url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_API_KEY_KOVAN}`,
+    accounts: [`${KOVAN_PRIVATE_KEY}`],
+  };
+}
+if (ALCHEMY_API_KEY_RINKEBY != "" && RINKEBY_PRIVATE_KEY != "") {
+  exportNetworks["rinkeby"] = {
+    url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY_RINKEBY}`,
+    accounts: [`${RINKEBY_PRIVATE_KEY}`],
+  };
+}
+if (SOKOL_PRIVATE_KEY != "") {
+  exportNetworks["sokol"] = {
+    url: `https://sokol.poa.network/`,
+    accounts: [`${SOKOL_PRIVATE_KEY}`],
+    gasPrice: 20000000000
+  }
+}
+
+if (MAINNET_PRIVATE_KEY != "") {
+  exportNetworks["gnosis"] = {
+    url: `https://rpc.gnosischain.com/`,
+    accounts: [`${MAINNET_PRIVATE_KEY}`],
+    gasPrice: 9000000000
+  }
 }
 
 module.exports = {
