6a773781334241cb1f048929ad55d1a3c6d8f4af
Alexander <shchepetov0@gmail.com>
2023-09-13 15:24:01 +0300

    Multi-release airdrop

--- contracts/AirDrop.sol
+++ contracts/AirDrop.sol
@@ -12,12 +12,11 @@ import "./interface/IAirDrop.sol";
  * @notice A contract for airdrop distribution using a Merkle tree.
  */
 contract AirDrop is OwnableUpgradeable, IAirDrop {
+    mapping(uint256 => Release) public releases;
+    mapping(uint256 => mapping(address => bool)) public isClaimed;
+
     IERC20 public token;
-
-    bytes32 public merkleRoot;
-    uint32 nonce;
-
-    mapping(address => uint32) public claimed;
+    uint256 public lastReleaseId;
 
     /**
      * @param  token_ Address of the token used in airdrops.
@@ -33,66 +32,69 @@ contract AirDrop is OwnableUpgradeable, IAirDrop {
      * @notice Release new airdrop of `amount` tokens.
      * @notice Removes the previous airdrop. Any funds that were not distributed in the previous airdrop will be used
      *         in the released airdrop.
-     * @param  merkleRoot_ The root of the Merkle tree where every leaf is a 52-byte record [address|reward] hashed
-     *         with keccak-256 (see https://en.wikipedia.org/wiki/Merkle_tree).                ^20B   ^32B
+     * @param  merkleRoot The root of the Merkle tree where every leaf is a 52-byte record [releaseId|address|reward]
+     *         hashed with keccak-256 (see https://en.wikipedia.org/wiki/Merkle_tree).        ^32B      ^20B   ^32B
      * @param  amount Total airdrop amount.
      * @param  data Bytes optional data to send along with the call.
      */
     function release(
-        bytes32 merkleRoot_,
+        bytes32 merkleRoot,
         uint256 amount,
         bytes calldata data
     ) external virtual onlyOwner {
         if (amount == 0) revert AmountMustNotBeZero();
 
-        _beforeRelease(merkleRoot_, amount, data);
+        _beforeRelease(merkleRoot, amount, data);
 
-        merkleRoot = merkleRoot_;
-        ++nonce;
+        uint256 releaseId = ++lastReleaseId;
+        releases[releaseId] = Release(merkleRoot, amount);
 
-        uint256 balance = token.balanceOf(address(this));
-        if (amount > balance)
-            TransferHelper.safeTransferFrom(
-                address(token),
-                msg.sender,
-                address(this),
-                amount - balance
-            );
+        TransferHelper.safeTransferFrom(
+            address(token),
+            msg.sender,
+            address(this),
+            amount
+        );
 
-        emit Released(amount);
+        emit Released(releaseId, merkleRoot, amount);
     }
 
     /**
-     * @notice Get `amount` of tokens from the current airdrop.
-     * @param  merkleProof Merkle's proof of rewarding `msg.sender` with `amount` of tokens in the current airdrop
+     * @notice Get `amount` of tokens from the airdrop `releaseId`.
+     * @param  releaseId The ID of the airdrop.
+     * @param  merkleProof Merkle's proof of rewarding `msg.sender` with `amount` of tokens in the airdrop `releaseId`
      *         (see https://en.wikipedia.org/wiki/Merkle_tree).
      * @param  amount The amount of tokens to be claimed.
      */
-    function claim(bytes32[] calldata merkleProof, uint256 amount)
-        external
-        override
-    {
+    function claim(
+        uint256 releaseId,
+        bytes32[] calldata merkleProof,
+        uint256 amount
+    ) external override {
+        Release memory release_ = releases[releaseId];
+        if (release_.merkleRoot == bytes32(0)) revert ReleaseDoesNotExist();
         if (
             !MerkleProof.verify(
                 merkleProof,
-                merkleRoot,
-                keccak256(abi.encodePacked(msg.sender, amount))
+                release_.merkleRoot,
+                keccak256(abi.encodePacked(releaseId, msg.sender, amount))
             )
         ) revert IncorrectData();
-        if (claimed[msg.sender] == nonce) revert AlreadyClaimed();
+        if (isClaimed[releaseId][msg.sender]) revert AlreadyClaimed();
+        if (amount > release_.balance) revert InsufficientReleaseBalance();
 
-        claimed[msg.sender] = nonce;
-
+        isClaimed[releaseId][msg.sender] = true;
+        releases[releaseId].balance = release_.balance - amount;
         TransferHelper.safeTransfer(address(token), msg.sender, amount);
 
-        emit Claimed(msg.sender, amount);
+        emit Claimed(releaseId, msg.sender, amount);
     }
 
     /**
      * @notice Hook that is called before each new airdrop release.
      */
     function _beforeRelease(
-        bytes32 merkleRoot_,
+        bytes32 merkleRoot,
         uint256 amount,
         bytes calldata data
     ) internal virtual {}
--- test/main-test.js
+++ test/main-test.js
@@ -25,74 +25,103 @@ const release = async (airDrop, owner, merkleRoot, amount) => {
   return await airDrop.connect(owner).release(merkleRoot, amount, []);
 };
 
-const claim = async (airDrop, user, merkleProof, amount) => {
-  return await airDrop.connect(user).claim(merkleProof, amount);
+const claim = async (airDrop, user, releaseId, merkleProof, amount) => {
+  return await airDrop.connect(user).claim(releaseId, merkleProof, amount);
 };
 
-const newRelease = async (n) => {
-  let users = [],
-    rewards = [],
+const newRelease = async (releaseId, users) => {
+  let rewards = [],
     leaves = [],
     totalReward = BigNumber.from(0);
-  let donor, user, reward;
 
-  [donor] = await ethers.getSigners();
-  for (let k = 0; k < n; k++) {
-    user = await ethers.Wallet.createRandom();
-    user = user.connect(ethers.provider);
-    await donor.sendTransaction({ to: user.address, value: ethers.utils.parseEther("1") });
-    reward = ethers.BigNumber.from(ethers.utils.randomBytes(4));
-
-    users.push(user);
+  for (let k = 0; k < users.length; k++) {
+    const reward = ethers.BigNumber.from(ethers.utils.randomBytes(4));
     rewards.push(reward);
-    leaves.push(toHash(user.address, reward));
+    leaves.push(toHash(releaseId, users[k].address, reward));
     totalReward = totalReward.add(reward);
   }
 
-  return [users, rewards, leaves, totalReward, new MerkleTree(leaves, web3.utils.keccak256, { sort: true })];
+  return [rewards, leaves, totalReward, new MerkleTree(leaves, web3.utils.keccak256, { sort: true })];
 };
 
-function toHash(address, amount) {
-  return web3.utils.keccak256(web3.utils.encodePacked({ t: "address", v: address }, { t: "uint256", v: amount }));
+function toHash(releaseId, address, amount) {
+  return web3.utils.keccak256(
+    web3.utils.encodePacked({ t: "uint256", v: releaseId }, { t: "address", v: address }, { t: "uint256", v: amount })
+  );
 }
 
 describe("AirDrop test", function () {
   let owner;
   let airDrop, usdt;
   let users, rewards, totalReward, leaves, tree;
+  let releaseId = 0;
 
   before(async () => {
+    users = await ethers.getSigners();
     [owner, user] = await ethers.getSigners();
     [airDrop, usdt] = await prepareStand(ethers, owner);
   });
   it("Should correctly reward users", async () => {
     for (let i = 0; i < 5; i++) {
-      const n = Math.floor(Math.random() * 31 + 1);
-      [users, rewards, leaves, totalReward, tree] = await newRelease(n);
+      [rewards, leaves, totalReward, tree] = await newRelease(++releaseId, users);
 
       await release(airDrop, owner, tree.getHexRoot(), totalReward);
-      for (let k = 0; k < n; k++) {
+      for (let k = 0; k < users.length; k++) {
         const user = users[k];
         const reward = rewards[k];
         const balance = await usdt.balanceOf(user.address);
-        await claim(airDrop, user, tree.getHexProof(leaves[k]), reward);
+        await claim(airDrop, user, releaseId, tree.getHexProof(leaves[k]), reward);
 
-        await expect(claim(airDrop, user, tree.getHexProof(leaves[k]), reward)).to.be.revertedWith("AlreadyClaimed()");
+        await expect(claim(airDrop, user, releaseId, tree.getHexProof(leaves[k]), reward)).to.be.revertedWith(
+          "AlreadyClaimed()"
+        );
         expect(await usdt.balanceOf(user.address)).to.be.equal(balance.add(reward));
-      }
-      [users, rewards, leaves] = await newRelease(3); // these users have no reward
-      for (let k = 0; k < 3; k++) {
-        const user = users[k];
-        const reward = rewards[k];
-
-        await expect(claim(airDrop, user, tree.getHexProof(leaves[k]), reward)).to.be.revertedWith("IncorrectData()");
       }
       console.log("      🌳");
     }
     expect(await usdt.balanceOf(airDrop.address)).to.be.equal(tokens(BigNumber.from(0)));
   });
+  it("Should reward users after new release", async () => {
+    [rewards, leaves, totalReward, tree] = await newRelease(++releaseId, users);
+    await release(airDrop, owner, tree.getHexRoot(), totalReward);
+
+    [rewards2, leaves2, totalReward2, tree2] = await newRelease(++releaseId, users);
+    await release(airDrop, owner, tree2.getHexRoot(), totalReward2);
+
+    for (let k = 0; k < users.length; k++) {
+      const user = users[k];
+      const reward = rewards[k];
+      const reward2 = rewards2[k];
+      const balance = await usdt.balanceOf(user.address);
+
+      await claim(airDrop, user, releaseId - 1, tree.getHexProof(leaves[k]), reward);
+      await claim(airDrop, user, releaseId, tree2.getHexProof(leaves2[k]), reward2);
+
+      expect(await usdt.balanceOf(user.address)).to.be.equal(balance.add(reward).add(reward2));
+    }
+  });
+  it("Should NOT reward for unexciting release", async () => {
+    [rewards, leaves, totalReward, tree] = await newRelease(++releaseId, users);
+    await release(airDrop, owner, tree.getHexRoot(), totalReward);
+    await expect(claim(airDrop, users[0], releaseId + 1, tree.getHexProof(leaves[0]), rewards[0])).to.be.revertedWith(
+      "ReleaseDoesNotExist"
+    );
+  });
+  it("Should NOT reward with incorrect data", async () => {
+    [rewards, leaves, totalReward, tree] = await newRelease(++releaseId, users);
+    await release(airDrop, owner, tree.getHexRoot(), totalReward);
+    await expect(claim(airDrop, users[0], releaseId, tree.getHexProof(leaves[1]), rewards[0])).to.be.revertedWith(
+      "IncorrectData"
+    );
+    await expect(claim(airDrop, users[0], releaseId, tree.getHexProof(leaves[0]), rewards[1])).to.be.revertedWith(
+      "IncorrectData"
+    );
+    await expect(claim(airDrop, users[1], releaseId, tree.getHexProof(leaves[0]), rewards[0])).to.be.revertedWith(
+      "IncorrectData"
+    );
+  });
   it("Should NOT release with zero amount", async () => {
-    [, , , , tree] = await newRelease(16);
+    [, , , tree] = await newRelease(++releaseId, users);
 
     await expect(release(airDrop, owner, tree.getHexRoot(), 0)).to.be.revertedWith("AmountMustNotBeZero()");
   });
@@ -113,7 +142,7 @@ describe("AirDrop test", function () {
 
       await usdt.approve(airDropRuleBased.address, tokens(10 ** 12));
 
-      [, , , , tree] = await newRelease(1);
+      [, , , tree] = await newRelease(++releaseId, users);
     });
     it("Check the rule hash set after initialization", async () => {
       expect(await airDropRuleBased.ruleHash()).to.be.equal(ruleHash);
